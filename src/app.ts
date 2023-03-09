cbd8c03b7f1b3ffd0d6b2f974ef30683bbd177cd
Michał Łowkiet <michallowkiet@gmail.com>
2023-03-09 14:25:59 +0100

    Set api doc as default route

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,39 @@
-import dotenv from "dotenv";
-import "express-async-errors";
+import dotenv from 'dotenv';
+import 'express-async-errors';
 
 //extra security packages
-import helmet from "helmet";
-import cors from "cors";
-import xss from "xss-clean";
-import rateLimiter from "express-rate-limit";
-import yaml from "yaml";
+import helmet from 'helmet';
+import cors from 'cors';
+import xss from 'xss-clean';
+import rateLimiter from 'express-rate-limit';
+import yaml from 'yaml';
 
-import fs from "fs";
-import swaggerUi from "swagger-ui-express";
-import express, { Request, Response } from "express";
+import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
+import express, { Request, Response } from 'express';
 
-import connectDB from "./db/ConnectDb.js";
-import Authentication from "./middleware/Authentication.js";
+import connectDB from './db/ConnectDb.js';
+import Authentication from './middleware/Authentication.js';
 
-import AuthRouter from "./routes/AuthRouter.js";
-import JobsRouter from "./routes/JobsRouter.js";
-import UserRouter from "./routes/UsersRouter.js";
+import AuthRouter from './routes/AuthRouter.js';
+import JobsRouter from './routes/JobsRouter.js';
+import UserRouter from './routes/UsersRouter.js';
 
-import NotFoundHandler from "./middleware/NotFoundHandler.js";
-import ErrorHandler from "./middleware/ErrorHandler.js";
+import NotFoundHandler from './middleware/NotFoundHandler.js';
+import ErrorHandler from './middleware/ErrorHandler.js';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import TestUserHandler from "./middleware/TestUserHandler.js";
+import TestUserHandler from './middleware/TestUserHandler.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
-
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-app.set("trust proxy", 1);
+app.set('trust proxy', 1);
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 request per windowMs
   message: {
-    msg: "Too many requests from this IP, please try again after 15 minutes.",
+    msg: 'Too many requests from this IP, please try again after 15 minutes.',
   },
 });
 
@@ -48,17 +42,13 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-const swaggerFile = fs.readFileSync("./swaggerDoc.yaml", "utf-8");
+const swaggerFile = fs.readFileSync('./swaggerDoc.yaml', 'utf-8');
 const swaggerDoc = yaml.parse(swaggerFile);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-app.use("/api/v1/auth", apiLimiter, AuthRouter);
-app.use("/api/v1/jobs", Authentication, JobsRouter);
-app.use("/api/v1/users", Authentication, TestUserHandler, UserRouter);
-
-app.get("*", (req: Request, res: Response) => {
-  res.send(path.resolve(__dirname, "../client/build", "index.html"));
-});
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use('/api/v1/auth', apiLimiter, AuthRouter);
+app.use('/api/v1/jobs', Authentication, JobsRouter);
+app.use('/api/v1/users', Authentication, TestUserHandler, UserRouter);
 
 app.use(NotFoundHandler);
 app.use(ErrorHandler);
@@ -67,9 +57,7 @@ const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
 
-    app.listen(PORT, () =>
-      console.log(`Server is listening on port ${PORT}...`)
-    );
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
   } catch (error) {
     console.log(error);
   }
